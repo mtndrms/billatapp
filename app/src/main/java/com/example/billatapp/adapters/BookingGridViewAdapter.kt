ec66f2a53ae5eaa63197b7fc049933ee92178477
Metin Durmus <mtn.drms@outlook.com>
2022-05-02 19:54:08 +0300

    screenshots

--- app/src/main/java/com/example/billatapp/adapters/BookingGridViewAdapter.kt
+++ app/src/main/java/com/example/billatapp/adapters/BookingGridViewAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
-import android.widget.Toast
 import com.example.billatapp.R
 
 class BookingGridViewAdapter(private val context: Context, private val seats: ArrayList<ImageView>) : BaseAdapter() {
